0f4271e1cac1d7c8c68228bd360f2aafedac4a1f
neriiavr <avrupalineriman@gmail.com>
2022-07-16 21:12:13 +0200

    checkbox implementation

--- script.js
+++ script.js
@@ -13,5 +13,35 @@ function addToDo() {
 
 // updating DOM section 
 function updateScreen() {
-    document.querySelector("ul").innerHTML = state.toDoList.join();
+  const idOfCheckbox = `checkbox ${state.toDoList[state.toDoList.length-1]}`;
+
+  document.querySelector("ul").innerHTML += (
+    `<li>
+      <input type="checkbox" id="${idOfCheckbox}">
+      ${state.toDoList[state.toDoList.length-1]}
+    </li>`
+  )
+
+  document.getElementById(idOfCheckbox).addEventListener("click", addDone)
 }
+
+function addDone(event) {
+  console.log(event)
+}
+
+
+// array.forEach(function (ulHTML) {
+//   console.log(ulHTML, state.toDoList);
+// });
+
+
+
+
+// `<li>${item}</li`
+
+// <li>buri</li>
+// <li>neri</li>
+
+// state.todoList = [buri, neri]
+
+// var ulHTML = `<li>eleman</li>`
